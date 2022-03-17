317dedb9b0acdd72d582e74546b39b3d21ff1c6d
anilfromdit <anilfromdit@gmail.com>
2022-03-18 03:55:20 +0530

    added payment component + integrated with stripe --test mode

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WebFont from "webfontloader";
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Home from "./component/Home/Home";
 import Footer from "./component/layout/Footer/Footer";
 import ProductDetails from "./component/Product/ProductDetails";
@@ -10,7 +10,7 @@ import Products from "./component/Product/Products";
 import LoginSignUp from "./component/User/LoginSignUp";
 import { loadUser } from "./actions/userActions";
 import UserOptions from "./component/layout/Header/UserOptions";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import Profile from "./component/User/Profile";
 import UpdateProfile from "./component/User/UpdateProfile";
 import MyNavbar from "./component/layout/Header/MyNavbar";
@@ -22,10 +22,21 @@ import Page404 from "./component/Misc/Page404";
 import Loader from "./component/layout/Loader/Loader";
 import Shipping from "./component/Cart/Shipping";
 import ConfirmOrder from "./component/Cart/ConfirmOrder";
+import Payment from "./component/Cart/Payment";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import store from "./store";
 
 function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+  const [stripeApiKey, setStripeApiKey] = useState("");
+
+  async function getStripeApiKey() {
+    const { data } = await axios.get("/api/v1/stripeapikey");
+
+    setStripeApiKey(data.stripeApiKey);
+  }
 
   useEffect(() => {
     WebFont.load({
@@ -33,13 +44,22 @@ function App() {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
-    dispatch(loadUser());
-  }, [dispatch]);
+    store.dispatch(loadUser());
+    getStripeApiKey();
+  }, []);
 
   return (
     <Router>
       <MyNavbar />
       {isAuthenticated && <UserOptions user={user} />}
+      {stripeApiKey && (
+        <Elements stripe={loadStripe(stripeApiKey)}>
+          <Routes>
+          <Route exact path="/process/payment" element={<Payment />} />
+          </Routes>
+        </Elements>
+      )}
+
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/testLoader" element={<Loader />} />
@@ -64,6 +84,9 @@ function App() {
           path="/password/updatePassword"
           element={<UpdatePassword />}
         />
+
+        {/* <Route exact path="/process/payment" element={<Payment/>} /> */}
+
         <Route exact path="/password/reset" element={<ForgotPassword />} />
         <Route exact path="*" element={<Page404 />} />
       </Routes>
